075d68a34ea650292f8c5fce935a15bde3459e15
tien94vg <tien94vg@gmail.com>
2020-06-17 16:56:02 +0700

    sever moi

---
+++ sever1.js
@@ -0,0 +1,13 @@
+var fs = require('fs')
+var express = require('express')
+var app = express()
+var path = require('path')
+
+
+app.use('/cookie', express.static(path.join(__dirname, './publict')))
+
+app.get('/', function(req,res) {
+    var link = path.join(__dirname, './htmlsv.html')
+    res.sendFile(link)
+})
+app.listen(3232)
